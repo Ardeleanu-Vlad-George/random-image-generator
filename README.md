bb1bcbe5a178c1f742fa40562226d8b5aca7617e
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-12-21 21:20:04 +0200

    Defined the broad lines of the code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Purpose
 Make project that generates a random image, with pixels having random values, based on the following parameters:
-- the sizes of the image, first two CLAs;
-- the name of the image, where it will get stored, the last CLA;
+- the sizes of the image, width first, than height, first two CLAs;
+- the name of the image, the file in which it will get stored, the last CLA;
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <cstddef>
+#include <ctime>
+
 
 int main(int argc, char *argv[]){
   if(4 != argc){
     std::cout<<"More arguments needed\n";
     return -1;
   }
+  int x = atoi(argv[1]), y = atoi(argv[2]);
+  srand(time(NULL));
+  sf::Image img;
+  img.create(x, y);
+  sf::Uint8 *data = (sf::Uint8*) img.getPixelsPtr();
+  int *data_int = (int*) img.getPixelsPtr();
+  for(int i=0; i < x*y; i++){
+    data_int[i] = rand();
+    data[4*i+3] = '\xff';
+  }
+  img.saveToFile(argv[3]);
+  return 0;
 }
