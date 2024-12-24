fdd83b389f9b8787cf1f5a15d3e5a1ba950bf0b7
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-12-24 20:00:18 +0200

    Added files

---
+++ inc/defs.hpp
@@ -0,0 +1,10 @@
+#ifndef _DEFS_HPP_
+#define _DEFS_HPP_
+
+#define HELP_STR "help"
+
+#define MIN_CLA_COUNT 
+
+#define MAX_CLA_COUNT 
+
+#endif//_DEFS_HPP_
---
+++ inc/io.hpp
@@ -0,0 +1,8 @@
+#ifndef _IO_HPP_
+#define _IO_HPP_
+
+#include <iostream>
+
+std::ostream& log_help(std::ostream&);
+
+#endif//_IO_HPP_
