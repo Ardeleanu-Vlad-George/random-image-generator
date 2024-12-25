2bde73868fa3f7ceb6c7239605fda87e6e6785e2
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-12-25 13:58:43 +0200

    Updating help message

--- src/io.cpp
+++ src/io.cpp
@@ -1,12 +1,17 @@
 #include "io.hpp"
 
 std::ostream& log_help(std::ostream& os){
-  os<<"\tWidth, height and file name, needed as first args\n";
-  os<<"\tNext dash options are permited\n";
-  os<<"\t\tr <value> - all reds the same, random if not activated\n";
-  os<<"\t\tg <value> - all greens the sam, random if not activated\n";
-  os<<"\t\tb <value> - all blues the sam, random if not activated\n";
-  os<<"\t\ta <value> - all alphas the same, if no value is given, the alphas are random, if not activated, the alphas are opaque\n";
+  os<<"First args must be the following three in the next order\nGive the following mandatory args in the next order\n";
+  os<<"\t1. width  of image\n";
+  os<<"\t2. height of image\n";
+  os<<"\t3. name   of storage file\n";
+  os<<"After that, the next options can be activated\n";
+  os<<"\tr <hex_value> - reds   of all pixels are the same, default, if value is missing\n";
+  os<<"\tg <hex_value> - greens of all pixels are the same, default if value is missing\n";
+  os<<"\tb <hex_value> - blues  of all pixels are the same, default if value is missing\n";
+  os<<"\ta <hex_value> - alphas of all pixels are the same, random values for each pixel, if value is missing\n";
+  os<<"If a color option is not activated, than the component is randomnly selected\n";
+  os<<"If the alpha value is not activated than the, if the alpha is op";
   return os;
 }
 
