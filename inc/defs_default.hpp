214b1047a41608c9ea6076beb19e7cf9982a40bb
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-12-25 23:23:18 +0200

    Added file that was forgotten in commit

---
+++ inc/defs_default.hpp
@@ -0,0 +1,16 @@
+#ifndef _DEFS_DEFAULT_HPP_
+#define _DEFS_DEFAULT_HPP_
+
+#define default_of_r '\xff'
+
+#define default_of_g '\xff'
+
+#define default_of_b '\xff'
+
+#define opaque '\xff'
+
+#define default_of_a  opaque
+
+#define def(comp) default_of_##comp
+
+#endif//_DEFS_DEFAULT_HPP_
