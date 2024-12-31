77fae224d11ab99ea63bb3b39f83541b6bda9721
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2024-12-31 17:15:02 +0200

    Commit from new machine, trying to see if changes can be pushed

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 echo "Hello Tests!"
-randimagen 400 400 data/rnd_1.png -r 50
-randimagen 400 400 data/rnd_2.png -r 150
+#randimagen 400 400 data/rnd_1.png -r 50
+#randimagen 400 400 data/rnd_2.png -r 150
 randimagen 400 400 data/rnd_3.png -g 50 -r 150 
